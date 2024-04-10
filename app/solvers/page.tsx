612d8551ef49af50e803bf9976e31f40a95f1c20
Josh Simpson <j054simpson@gmail.com>
2024-04-10 01:15:44 -0700

    Slight updates.

--- app/solvers/page.tsx
+++ app/solvers/page.tsx
@@ -1,10 +1,15 @@
+import React from "react";
+
+/*
 async function getSolversList(){
     const res = await fetch('');
     const data = await res.json();
     return data?.items as any[];
 }
+*/
 
 export default function SolverPage() {
+
     return (
         <form className="w-full max-w-lg">
             <div className="flex flex-wrap -mx-3 mb-6">
@@ -33,8 +38,7 @@ export default function SolverPage() {
                 </div>
                 <div className="w-full md:w-1/2 px-3 py-4 mb-6 md:mb-0">
                     <button
-                        className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-                        type="button">
+                        className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
                         Add Solver to List
                     </button>
                 </div>
