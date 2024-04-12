4fe278d23c3ea02168884819c50508e3c447427d
joshsimpsondev <108380425+joshsimpsondev@users.noreply.github.com>
2024-04-12 15:01:47 -0700

    Changing Routing, starting to display table.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-async function getData() {
-  const res = await fetch('http://localhost:3000/api');
+import SolverTable from "./table/solver-table";
+async function getSolverListData() {
+  const res = await fetch('http://localhost:3000/api?action=solverlist');
 
   if(!res.ok){
     throw new Error("Failed to Fetch Data");
@@ -9,10 +10,11 @@ async function getData() {
 }
 
 export default async function HomePage() {
-  const data = await getData();
+  const data = await getSolverListData();
   return (
     <main>
       <label> {data} </label>
+      <SolverTable></SolverTable>
     </main>
   );
 }
---
+++ app/table/solver-table.tsx
@@ -0,0 +1,6 @@
+'use client'
+export default function SolverTable(){
+    return (
+        <div>Hello</div>
+    )
+}
