c253f7b9f17711a6c52b0da96dd4f59f261d87fe
joshsimpsondev <108380425+joshsimpsondev@users.noreply.github.com>
2024-04-15 16:18:25 -0700

    BugFix

--- app/api/solvers/route.ts
+++ app/api/solvers/route.ts
@@ -1,4 +1,6 @@
 import {getSolversList} from "@/app/solvers/solver-list";
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
 
 function solversList() {
     return Response.json(getSolversList());
